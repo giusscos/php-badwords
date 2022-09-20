6f519373df82869d6c3e32f34644e6472be1453d
giusscos <giu.cosenza08@gmail.com>
2022-09-20 14:25:58 +0200

    Aggiunta censura

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 $description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eum reprehenderit perspiciatis?';
-$len_description = strlen($description);
+// $len_description = strlen($description);
+
+$user_input = $_GET['word'];
+$censured_chars = '***';
+
+$censured_str = str_replace($user_input, $censured_chars, $description);
+$len_description = strlen($censured_str);
 ?>
 
 <h1> Hello PHP! </h1>
-<p> Questo paragrafo >> "<?php echo $description ?>" ha una lunghezza di: <?php echo $len_description ?> caratteri </p>
+<p> Questo paragrafo >> "<?php echo $censured_str ?>" ha una lunghezza di: <?php echo $len_description ?> caratteri </p>
