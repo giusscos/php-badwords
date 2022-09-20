193b58cd044bac796bf0e8b6096d79a938006b4f
giusscos <giu.cosenza08@gmail.com>
2022-09-20 14:20:18 +0200

    Aggiuinta paragrafo con lunghezza

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<?php 
-    echo 'Hello Wolrd!';
+<?php
+$description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi eum reprehenderit perspiciatis?';
+$len_description = strlen($description);
+?>
+
+<h1> Hello PHP! </h1>
+<p> Questo paragrafo >> "<?php echo $description ?>" ha una lunghezza di: <?php echo $len_description ?> caratteri </p>
